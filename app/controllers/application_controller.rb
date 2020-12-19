8b64c6e98f60fc749b079f84af188629ee60a0e1
Tonon Abdul-Shafiu Mahmud <tononabdulshafiumahmud@gmail.com>
2020-12-19 15:41:53 -0800

    Devise sign up and error helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user! 
+  layout :layout_by_resource 
+ protected 
+  def layout_by_resource 
+    if devise_controller? 
+      "devise" 
+   else 
+      "application" 
+   end 
+ end 
 end
+ 
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
-module ApplicationHelper
+module ApplicationHelper 
+  def bootstrap_class_for flash_type  
+    { 
+         success: "alert-success", 
+         error: "alert-danger", 
+         alert: "alert-warning", 
+         notice: "alert-info" 
+          }[flash_type.to_sym] || flash_type.to_s 
+        end 
+        
+def flash_messages(_opts = {}) 
+# return if flash[0][0] == :form_error 
+  flash.each do |msg_type, message| 
+        next if msg_type == :form_error 
+        concat(content_tag(:div, message.html_safe, { 
+        class: "alert #{bootstrap_class_for(msg_type)} fade show"}) do 
+        concat content_tag(:button, 'x', class: "close", data: { 
+        dismiss: 'alert' 
+        }) 
+        concat message.html_safe 
+        end) 
+        end 
+  nil 
+end 
 end
